3c2760361c054c65484599e3dc99ba9a7a0d91c7
sagarsomwanshi <sagarsomwanshi13@gmail.com>
2023-03-31 13:21:00 +0530

    Added : contact Validation by Regex

--- AddressBook.js
+++ AddressBook.js
@@ -1,20 +1,14 @@
 const ContactDetails = require("./ContactDetails.js");
 const prompt = require('prompt-sync')();
 
-let contactInfo = new ContactDetails("Sagar","Somwanshi","Anuvishwa Society","Solapur","Maharashtra","413004","8483820145"
-                  ,"sagarsomwanshi13@gmail.com");
-console.log(contactInfo.toString());
-            
+let contactInfo = new ContactDetails();
+    contactInfo.fName = prompt("Enter First Name : ");
+    contactInfo.lName = prompt("Enter Last Name : ");
+    contactInfo.addressName = prompt("Enter Address : ");
+    contactInfo.cityName = prompt("Enter City : ");
+    contactInfo.stateName = prompt("Enter State : ");
+    contactInfo.zipNumber = prompt("Enter Zip : ");
+    contactInfo.phoneNo = prompt("Enter Phone Number : ");
+    contactInfo.emailID = prompt("Enter Email : ");
+    console.log(contactInfo.toString());
 
-contactInfo.firstName = "Akash";
-contactInfo.lastName = "Somwanshi";
-contactInfo.address = "Anuvishwa Society";
-contactInfo.city = "Solapur";
-contactInfo.state = "Maharashtra";
-contactInfo.zip = "413004";
-contactInfo.phoneNumber = "8087364342";
-contactInfo.email = "akash@gmail.com";
-
-contactInfo.
-
-console.log(contactInfo.toString());
